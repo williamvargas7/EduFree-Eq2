1ce09792562084d69fc508ee8eb66759f2856928
williamvargas7 <86165574+williamvargas7@users.noreply.github.com>
2021-11-11 23:12:56 -0500

    Correccion Entity modelo UsuarioPorGrupo

--- backend-edufree-eq2/src/models/usuario-por-grupo.model.ts
+++ backend-edufree-eq2/src/models/usuario-por-grupo.model.ts
@@ -1,7 +1,7 @@
-import {Model, model, property} from '@loopback/repository';
+import {Entity, model, property} from '@loopback/repository';
 
 @model()
-export class UsuarioPorGrupo extends Model {
+export class UsuarioPorGrupo extends Entity {
   @property({
     type: 'string',
     required: true,
