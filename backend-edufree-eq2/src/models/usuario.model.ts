dbb0c8a89ca7a5e6c7fe8578a1bb0b4e7d346fab
Lorena Benítez <78233707+LorenaBtz@users.noreply.github.com>
2021-11-11 21:11:42 -0500

    Modelo Usuario

---
+++ backend-edufree-eq2/src/models/usuario.model.ts
@@ -0,0 +1,106 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Usuario extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  usuario: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  contrasenia: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombres: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  apellidos: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  tipoDocumento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  numIdentificacion: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  codigoUniversitario: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  idPrograma: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  ciclo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  genero: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  rol: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  discapacidad: boolean;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  creditosAcumulados: number;
+
+
+  constructor(data?: Partial<Usuario>) {
+    super(data);
+  }
+}
+
+export interface UsuarioRelations {
+  // describe navigational properties here
+}
+
+export type UsuarioWithRelations = Usuario & UsuarioRelations;
