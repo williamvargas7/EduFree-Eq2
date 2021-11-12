5a9e8d12ceecb1eb78c93b68d2190a9b1bdfc323
Lorena Benítez <78233707+LorenaBtz@users.noreply.github.com>
2021-11-11 21:18:22 -0500

    Modulo Perfil

--- backend-edufree-eq2/src/models/index.ts
+++ backend-edufree-eq2/src/models/index.ts
@@ -1,3 +1,4 @@
 export * from './programa-academico.model';
 export * from './asignatura.model';
 export * from './usuario.model';
+export * from './perfil.model';
---
+++ backend-edufree-eq2/src/models/perfil.model.ts
@@ -0,0 +1,45 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Perfil extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'object',
+    required: true,
+  })
+  permisos: object;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  modulos: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Perfil>) {
+    super(data);
+  }
+}
+
+export interface PerfilRelations {
+  // describe navigational properties here
+}
+
+export type PerfilWithRelations = Perfil & PerfilRelations;
