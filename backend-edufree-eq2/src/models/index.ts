b1e86440aeb4f176b5b35b1ca392c7a1098a42f7
Lorena Benítez <78233707+LorenaBtz@users.noreply.github.com>
2021-11-17 19:43:02 -0500

    Eliminacion BackEnd Inicial

--- backend-edufree-eq2/src/models/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './programa-academico.model';
-export * from './asignatura.model';
-export * from './usuario.model';
-export * from './perfil.model';
-export * from './grupos.model';
-export * from './usuario-por-grupo.model';
--- backend-edufree-eq2/src/repositories/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './asignatura.repository';
-export * from './grupos.repository';
-export * from './perfil.repository';
-export * from './programa-academico.repository';
-export * from './usuario-por-grupo.repository';
-export * from './usuario.repository';
