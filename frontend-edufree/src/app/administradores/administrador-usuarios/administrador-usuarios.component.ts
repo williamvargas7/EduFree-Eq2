27674674c5cfa3a09e133dbda4e4318157bbd9d7
williamvargas7 <86165574+williamvargas7@users.noreply.github.com>
2021-12-07 00:57:57 -0500

    Metodos CRUD para actualizar usuario

--- frontend-edufree/src/app/administradores/administrador-usuarios/administrador-usuarios.component.ts
+++ frontend-edufree/src/app/administradores/administrador-usuarios/administrador-usuarios.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { BackendService } from 'src/services/backend.service';
 import { Md5 } from 'ts-md5';
+import swal from 'sweetalert2';
 
 interface Usuario {
   nombresUsuario: string,
@@ -36,24 +37,25 @@ export class AdministradorUsuariosComponent implements OnInit {
   listaUsuarios: Usuario[] = [];
   listaProgramas: Programa[] = [];
   listaRoles: Rol[] = [];
+  idUsuarioEdit = '';
+  modoCrud = 'adicion';
 
   constructor(
     private fb: FormBuilder,
     private Backend: BackendService
-  )
-  {
-    this.formUsuario=this.fb.group(
+  ) {
+    this.formUsuario = this.fb.group(
       {
         nombresUsuario: ['', Validators.required],
-        apellidosUsuario: ['',  Validators.required],
-        tipoIdentificacion: ['',  Validators.required],
-        noIdentificacion: ['',  Validators.required],
-        correoElectronico: ['',  Validators.compose([Validators.email,Validators.required])],
-        telefono: ['',  Validators.required],
-        programaAcademicoId: ['',  Validators.required],
-        rol: ['',  Validators.required],
-        usuario: ['',  Validators.required],
-        contrasenia: ['',  Validators.required]
+        apellidosUsuario: ['', Validators.required],
+        tipoIdentificacion: ['', Validators.required],
+        noIdentificacion: ['', Validators.required],
+        correoElectronico: ['', Validators.compose([Validators.email, Validators.required])],
+        telefono: ['', Validators.required],
+        programaAcademicoId: ['', Validators.required],
+        rol: ['', Validators.required],
+        usuario: ['', Validators.required],
+        contrasenia: ['', Validators.required]
       }
     );
     this.obtenerUsuarios();
@@ -87,14 +89,14 @@ export class AdministradorUsuariosComponent implements OnInit {
     this.Backend.get('/programas-academicos').subscribe(
       {
         next: (data) => {
-         // alert(data+"Datos Obtenidos correctamente");
+          // alert(data+"Datos Obtenidos correctamente");
           this.listaProgramas = data;
         },
         error: (err) => {
           //alert(err);
         },
         complete: () => {
-         // alert("Completado");
+          // alert("Completado");
         }
       }
     );
@@ -104,39 +106,94 @@ export class AdministradorUsuariosComponent implements OnInit {
     this.Backend.get('/perfiles').subscribe(
       {
         next: (data) => {
-         // alert(data+"Datos Obtenidos correctamente");
+          // alert(data+"Datos Obtenidos correctamente");
           this.listaRoles = data;
         },
         error: (err) => {
           //alert(err);
         },
         complete: () => {
-         // alert("Completado");
+          // alert("Completado");
         }
       }
     );
   }
 
-  crearUsuario():void{
-    const usuarioNuevo =this.formUsuario.getRawValue();
+  iniciarAdicion(): void {
+    this.modoCrud = 'adicion';
+  }
+
+  crearUsuario(): void {
+    const usuarioNuevo = this.formUsuario.getRawValue();
     const contraseniaEncriptada = Md5.hashStr(this.formUsuario.controls.contrasenia.value)
-    usuarioNuevo.contrasenia= contraseniaEncriptada;
-    usuarioNuevo.fechaCreacion= "2021-12-04T06:09:59.036Z";
-    usuarioNuevo.perfilId= "string";
-    this.Backend.postRequest('usuarios',JSON.stringify(usuarioNuevo)).subscribe(
+    usuarioNuevo.contrasenia = contraseniaEncriptada;
+    usuarioNuevo.fechaCreacion = "2021-12-04T06:09:59.036Z";
+    usuarioNuevo.perfilId = "string";
+    this.Backend.postRequest('usuarios', JSON.stringify(usuarioNuevo)).subscribe(
       {
-        next: (data) => { 
-          
+        next: (data) => {
+          this.listaUsuarios.unshift(data);
+          swal.fire('Felicidades', 'Usuario Creado', 'success');
+          this.formUsuario.reset();
           console.log(data);
-          
+
         },
         error: (err) => {
           console.log(err);
         },
         complete: () => {
-          
+
         }
       }
+    );
+  }
+
+
+  IniciarEdicion(usuario: any): void {
+    this.formUsuario.patchValue(usuario);
+    this.idUsuarioEdit = usuario.id;
+    this.modoCrud = 'edicion';
+  }
+
+  Editar(): void {
+    const nuevoUsuario = this.formUsuario.getRawValue();
+    const contraseniaEncriptada = Md5.hashStr(this.formUsuario.controls.contrasenia.value)
+    nuevoUsuario.contrasenia = contraseniaEncriptada;
+    this.Backend.patchRequest('usuarios', this.idUsuarioEdit, nuevoUsuario).subscribe(
+      {
+        next: (data) => {
+          this.obtenerUsuarios();
+          swal.fire('Felicidades', 'Usuario Editado', 'success');
+        },
+        error: (err) => {
+          console.log(err);
+        },
+        complete: () => {
+
+        }
+      }
+    );
+  }
+
+  Limpiar(): void {
+    this.formUsuario.reset();
+  }
+
+  EmininarUsuario(usuario: any) {
+    this.Backend.deleteRequest('usuarios', usuario.id).subscribe(
+      {
+        next: () => {
+          this.obtenerUsuarios();
+          swal.fire('!!!', 'Usuario Eliminado ' + usuario.nombresUsuario + ' ' + usuario.apellidosUsuario, 'success');
+        },
+        error: (err) => {
+          console.log(err);
+        },
+        complete: () => {
+
+        }
+      }
+
     );
   }
 
