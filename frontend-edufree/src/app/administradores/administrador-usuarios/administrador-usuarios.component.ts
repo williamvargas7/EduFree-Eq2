fd53e9db286140fe7f8924822c843a6533e81324
williamvargas7 <86165574+williamvargas7@users.noreply.github.com>
2021-12-08 22:05:41 -0500

    Metodo CRUD para crear usuario

--- frontend-edufree/src/app/administradores/administrador-usuarios/administrador-usuarios.component.ts
+++ frontend-edufree/src/app/administradores/administrador-usuarios/administrador-usuarios.component.ts
@@ -1,41 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { timestamp } from 'rxjs';
 import { BackendService } from 'src/services/backend.service';
 import { Md5 } from 'ts-md5';
 import swal from 'sweetalert2';
 
 interface Usuario {
-  nombresUsuario: string,
-  apellidosUsuario: string,
-  tipoIdentificacion: string,
-  noIdentificacion: string,
-  correoElectronico: string,
-  telefono: string,
-  programaAcademicoId: string,
-  rol: string,
-  usuario: string,
-  contrasenia: string,
-  fechaCreacion: string
+  id: string;
+  nombresUsuario: string;
+  apellidosUsuario: string;
+  tipoIdentificacion: string;
+  noIdentificacion: string;
+  correoElectronico: string;
+  telefono: string;
+  programaAcademicoId: string;
+  rol: string;
+  usuario: string;
+  contrasenia: string;
+  fechaCreacion: string;
 }
 
 interface Programa {
-  id: string,
-  nombrePrograma: string
+  id: string;
+  nombrePrograma: string;
 }
 
 interface Rol {
-  id: string,
-  nombrePerfil: string
+  id: string;
+  nombrePerfil: string;
 }
 
 @Component({
   selector: 'app-administrador-usuarios',
   templateUrl: './administrador-usuarios.component.html',
-  styleUrls: ['./administrador-usuarios.component.scss']
+  styleUrls: ['./administrador-usuarios.component.scss'],
 })
 export class AdministradorUsuariosComponent implements OnInit {
-
   formUsuario: any;
   listaUsuarios: Usuario[] = [];
   listaProgramas: Programa[] = [];
@@ -43,110 +42,106 @@ export class AdministradorUsuariosComponent implements OnInit {
   idUsuarioEdit = '';
   modoCrud = 'adicion';
 
-  constructor(
-    private fb: FormBuilder,
-    private Backend: BackendService
-  ) {
-    this.formUsuario = this.fb.group(
-      {
-        nombresUsuario: ['', Validators.required],
-        apellidosUsuario: ['', Validators.required],
-        tipoIdentificacion: ['', Validators.required],
-        noIdentificacion: ['', Validators.required],
-        correoElectronico: ['', Validators.compose([Validators.email, Validators.required])],
-        telefono: ['', Validators.required],
-        programaAcademicoId: ['', Validators.required],
-        rol: ['', Validators.required],
-        usuario: ['', Validators.required],
-        contrasenia: ['', Validators.required]
-      }
-    );
+  constructor(private fb: FormBuilder, private Backend: BackendService) {
+    this.formUsuario = this.fb.group({
+      nombresUsuario: ['', Validators.required],
+      apellidosUsuario: ['', Validators.required],
+      tipoIdentificacion: ['', Validators.required],
+      noIdentificacion: ['', Validators.required],
+      correoElectronico: [
+        '',
+        Validators.compose([Validators.email, Validators.required]),
+      ],
+      telefono: ['', Validators.required],
+      programaAcademicoId: ['', Validators.required],
+      rol: ['', Validators.required],
+      usuario: ['', Validators.required],
+      contrasenia: ['', Validators.required],
+      fechaCreacion: ['', Validators.required],
+      perfilId: ['', Validators.required],
+    });
     this.obtenerUsuarios();
     this.obtenerProgramas();
     this.obtenerRoles();
-    // this.limpiarFomrulario();
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   obtenerUsuarios(): void {
-
-    this.Backend.get('usuarios').subscribe(
-      {
-        next: (data) => {
-          this.listaUsuarios = data;
-          console.log(data);
-        },
-        error: (e) => {
-          console.log('error');
-        },
-        complete: () => {
-          console.log('completo');
-        }
-      }
-    );
+    this.Backend.get('usuarios').subscribe({
+      next: (data) => {
+        this.listaUsuarios = data;
+        console.log(data);
+      },
+      error: (e) => {
+        console.log('error');
+      },
+      complete: () => {
+        console.log('completo');
+      },
+    });
   }
 
   obtenerProgramas(): void {
-    this.Backend.get('/programas-academicos').subscribe(
-      {
-        next: (data) => {
-          // alert(data+"Datos Obtenidos correctamente");
-          this.listaProgramas = data;
-        },
-        error: (err) => {
-          //alert(err);
-        },
-        complete: () => {
-          // alert("Completado");
-        }
-      }
-    );
+    this.Backend.get('/programas-academicos').subscribe({
+      next: (data) => {
+        // alert(data+"Datos Obtenidos correctamente");
+        this.listaProgramas = data;
+      },
+      error: (err) => {
+        //alert(err);
+      },
+      complete: () => {
+        // alert("Completado");
+      },
+    });
   }
 
   obtenerRoles(): void {
-    this.Backend.get('/perfiles').subscribe(
-      {
-        next: (data) => {
-          // alert(data+"Datos Obtenidos correctamente");
-          this.listaRoles = data;
-        },
-        error: (err) => {
-          //alert(err);
-        },
-        complete: () => {
-          // alert("Completado");
-        }
-      }
-    );
-  }
-
-  crearUsuario():void{
-    const usuarioNuevo = this.formUsuario.getRawValue();
-    usuarioNuevo.fechaCreacion= timestamp;
-    usuarioNuevo.perfilId= usuarioNuevo.rol;
-    usuarioNuevo.rol= "string";
-    usuarioNuevo.programaAcademicoId= "string";
-
-    this.Backend.postRequest('usuarios',JSON.stringify(usuarioNuevo)).subscribe(
-      {
-        next: (data) => {
-          console.log(data);
-        },
-        error: (err) => {
-          console.log(err);
-        },
-        complete: () => {
-          console.log('complete');
-        }
-      }
-
-    );
+    this.Backend.get('/perfiles').subscribe({
+      next: (data) => {
+        // alert(data+"Datos Obtenidos correctamente");
+        this.listaRoles = data;
+      },
+      error: (err) => {
+        //alert(err);
+      },
+      complete: () => {
+        // alert("Completado");
+      },
+    });
   }
 
   iniciarAdicion(): void {
     this.modoCrud = 'adicion';
+  }
+
+  crearUsuario(): void {
+    const usuarioNuevo = this.formUsuario.getRawValue();
+    const contraseniaEncriptada = Md5.hashStr(
+      this.formUsuario.controls.contrasenia.value
+    );
+    usuarioNuevo.contrasenia = contraseniaEncriptada;
+    usuarioNuevo.foto =
+      'https://cdn-icons-png.flaticon.com/512/2784/2784445.png';
+
+    usuarioNuevo.perfilId = usuarioNuevo.rol;
+
+    this.Backend.postRequest(
+      'usuarios',
+      JSON.stringify(usuarioNuevo)
+    ).subscribe({
+      next: (data) => {
+        this.listaUsuarios.unshift(data);
+        swal.fire('Felicidades', 'Usuario Creado', 'success');
+        this.formUsuario.reset();
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {},
+    });
   }
 
   IniciarEdicion(usuario: any): void {
@@ -156,23 +151,26 @@ export class AdministradorUsuariosComponent implements OnInit {
   }
 
   Editar(): void {
-    const nuevoUsuario = this.formUsuario.getRawValue();
-    const contraseniaEncriptada = Md5.hashStr(this.formUsuario.controls.contrasenia.value)
-    nuevoUsuario.contrasenia = contraseniaEncriptada;
-    this.Backend.patchRequest('usuarios', this.idUsuarioEdit, nuevoUsuario).subscribe(
-      {
-        next: (data) => {
-          this.obtenerUsuarios();
-          swal.fire('Felicidades', 'Usuario Editado', 'success');
-        },
-        error: (err) => {
-          console.log(err);
-        },
-        complete: () => {
+    const editarUsuario = this.formUsuario.getRawValue();
+    const contraseniaEncriptada = Md5.hashStr(this.formUsuario.controls.contrasenia.value);
+    editarUsuario.contrasenia = contraseniaEncriptada;
 
-        }
-      }
-    );
+    this.Backend.patchRequest(
+      'usuarios',
+      this.idUsuarioEdit,
+      editarUsuario
+    ).subscribe({
+      next: (data) => {
+        this.obtenerUsuarios();
+        swal.fire('Felicidades', 'Usuario Editado', 'success');
+        this.formUsuario.reset();
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {},
+    });
   }
 
   Limpiar(): void {
@@ -180,20 +178,22 @@ export class AdministradorUsuariosComponent implements OnInit {
   }
 
   EmininarUsuario(usuario: any) {
-    this.Backend.deleteRequest('usuarios', usuario.id).subscribe(
-      {
-        next: () => {
-          this.obtenerUsuarios();
-          swal.fire('!!!', 'Usuario Eliminado ' + usuario.nombresUsuario + ' ' + usuario.apellidosUsuario, 'success');
-        },
-        error: (err) => {
-          console.log(err);
-        },
-        complete: () => {
-
-        }
-      }
-
-    );
+    this.Backend.deleteRequest('usuarios', usuario.id).subscribe({
+      next: () => {
+        this.obtenerUsuarios();
+        swal.fire(
+          '!!!',
+          'Usuario Eliminado ' +
+            usuario.nombresUsuario +
+            ' ' +
+            usuario.apellidosUsuario,
+          'success'
+        );
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {},
+    });
   }
 }
